a94336f11e288dfe8de786956607d856662fe7cd
Carolina Alvarez <carol02.carolina@gmail.com>
2022-05-24 19:53:35 -0500

    pruebas unitarias

---
+++ src/test/java/pruebasunitarias/TextCaseConvert.java
@@ -0,0 +1,38 @@
+package pruebasunitarias;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TextCaseConvert {
+    public String convertirTextoMayusculas(String text){
+        return text.toUpperCase();
+    }
+    public String convertirTextoMinusculas(String text){
+        return text.toLowerCase();
+    }
+    @Test
+    public void validarMayusculas(){
+        System.out.println("\n####################### MAYÚSCULAS #############################");
+        String expectedResult = "INTRO A TESTNG";
+        String inputText = "Intro a TestNG";
+        TextCaseConvert textCaseConvert = new TextCaseConvert();
+        String actualResult = textCaseConvert.convertirTextoMayusculas(inputText);
+        System.out.println("Input text: " + inputText);
+        System.out.println("Expected result: " + expectedResult + "\nActual result: " + actualResult);
+        System.out.println("#################################################################");
+        Assert.assertEquals(actualResult,expectedResult);
+    }
+
+    @Test
+    public void validarMinusculas(){
+        System.out.println("\n####################### MINÚSCULAS #############################");
+        String expectedResult = "intro a testng";
+        String inputText = "Intro a TestNG";
+        TextCaseConvert textCaseConvert = new TextCaseConvert();
+        String actualResult = textCaseConvert.convertirTextoMayusculas(inputText);
+        System.out.println("Input text: " + inputText);
+        System.out.println("Expected result: " + expectedResult + "\nActual result: " + actualResult);
+        System.out.println("#################################################################");
+        Assert.assertEquals(actualResult,expectedResult);
+    }
+}
