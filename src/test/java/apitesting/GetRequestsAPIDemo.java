228e3fea101530b8319d29a14f1f585d76337bd3
Carolina Alvarez <carol02.carolina@gmail.com>
2022-05-31 19:49:49 -0500

    nuevas dependencias

---
+++ src/test/java/apitesting/GetRequestsAPIDemo.java
@@ -0,0 +1,53 @@
+package apitesting;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utils.ExecuteRequest;
+import utils.GlobalVariables;
+
+import static io.restassured.RestAssured.given;
+
+public class GetRequestsAPIDemo {
+
+    @Test
+    public void buildGetRequest(){
+        RestAssured.baseURI = "http://localhost:5000";
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/users/findbyid/14")
+                .then()
+                .extract().response();
+
+        System.out.println(response.getBody().prettyPrint());
+    }
+
+    @Test(groups = {"getAll","get"})
+    public void getAllUsers(){
+        String url = GlobalVariables.apiHost +  "/users/all";
+        Response response =  ExecuteRequest.makeGetRequest(url);
+        System.out.println(response.asString());
+        JSONArray jsonArray = new JSONArray(response.asString());
+        System.out.println(jsonArray.toString(10));
+        if(jsonArray.length() > 10){
+            Assert.fail("Length should be 10");
+        }
+    }
+
+    @Test
+    public void findById(){
+        String url = GlobalVariables.apiHost +  "/users/findbyid/14";
+        Response response = ExecuteRequest.makeGetRequest(url);
+        System.out.println(response.asString());
+        JSONObject jsonArray = new JSONObject(response.asString());
+        System.out.println(jsonArray.toString(10));
+        Assert.assertEquals(response.statusCode(), 200);
+        Assert.assertEquals(response.jsonPath().get("id").toString(), "14");
+    }
+
+}
